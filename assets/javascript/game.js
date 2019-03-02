d03fbbd65fd609b7f65ea6a8d9280707fdcbb839
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-03-02 00:19:11 -0500

    all but reseting of crystals is functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,103 @@
 $(document).ready(function() {
 
 // Define global variables.
-var gemValue = [];
+var crystalValue = [];
 var wins = 0;
 var losses = 0;
 var score = 0;
 var targetNumber;
-var gemPictures = ["assets/images/gemOne.jpg", "assets/images/gemTwo.jpg", "assets/images/gemThree.jpg", "assets/images/gemFour.jpg"];
+var crystalPictures = ["assets/images/gemOne.jpg", "assets/images/gemTwo.jpg", "assets/images/gemThree.jpg", "assets/images/gemFour.jpg"];
 
-function gemCalc() {
-    for (i = 0; i < gemPictures.length; i++) {
-        var worth = Math.floor(Math.random() * 12) + 1;
-        gemValue.push(worth);
+function crystalCalc() {
+    // crystalValue.length = 0;
+    for (i = 0; i < crystalPictures.length; i++) {
+        crystalValue[i] = Math.floor(Math.random() * 12) + 1;
+        // crystalValue.push(worth);
     }
  
-    // if (gemValue[0] % 2 === gemValue[1] % 2 === gemValue[2] % 2 === gemValue[3] % 2 === 100) {
-    //     gemCalc();
+    // if (crystalValue[0] % 2 === crystalValue[1] % 2 === crystalValue[2] % 2 === crystalValue[3] % 2 === 100) {
+    //     crystalCalc();
     // }
 
 }
 
 function newRound() {
     targetNumber = Math.floor(Math.random() * 102) + 19;
-
-    gemCalc()
-}
-
-function assignment() {
-    for (var i = 0; i < gemPictures.length; i++) {
+    $("#target-number").text("Target Number: " + targetNumber);
+    crystalCalc();
+    // for (j = 0; j < crystalPictures.length; j++) {
+    //     var worth = Math.floor(Math.random() * 12) + 1;
+    //     crystalValue.push(worth);
+    // }
+    for (var i = 0; i < crystalPictures.length; i++) {
         var imageCrystal = $("<img>");
         imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", gemPictures[i]);
-        imageCrystal.attr("data-crystalvalue", gemValue[i]);
+        imageCrystal.attr("src", crystalPictures[i]);
+        imageCrystal.attr("data-crystalvalue", crystalValue[i]);
         $("#crystals").append(imageCrystal);
     }
-
-
-
 }
 
+function subRound() {
+    score = 0;
+    $("#score").text("Score: " + score);
+    targetNumber = Math.floor(Math.random() * 102) + 19;
+    $("#target-number").text("Target Number: " + targetNumber);
+    // crystalCalc();
+    for (var i = 0; i < crystalPictures.length; i++) {
+        var imageCrystal = $("<img>");
+        imageCrystal.removeAttr("data-crystalvalue");
+        // imageCrystal.addClass("crystal-image");
+        // imageCrystal.attr("src", crystalPictures[i]);
+        imageCrystal.attr("data-crystalvalue", crystalValue[i]);
+        $("#crystals").append(imageCrystal);
+    }
+}
+
+// added to newRound()
+// function assignment() {
+//     for (var i = 0; i < crystalPictures.length; i++) {
+//         var imageCrystal = $("<img>");
+//         imageCrystal.addClass("crystal-image");
+//         imageCrystal.attr("src", crystalPictures[i]);
+//         imageCrystal.attr("data-crystalvalue", crystalValue[i]);
+//         $("#crystals").append(imageCrystal);
+//     }
+// }
+
+//beginning of game
+$("#win-count").text("Wins: " + wins);
+$("#loss-count").text("Losses: " + losses);
 newRound();
-assignment();
+// assignment();
+
+$(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    score += crystalValue;
+        console.log(score);
+    $("#score").text("Score: " + score);
+
+    if (score === targetNumber) {
+        $("#winloseAlert").text("You win!");
+        wins++;
+        $("#win-count").text("Wins: " + wins);
+        subRound();
+      }
+  
+      else if (score >= targetNumber) {
+        $("#winloseAlert").text("You lose.");
+        losses++;
+        $("#loss-count").text("Losses: " + losses);
+        subRound();
+      }
+  
+    });
+
+
 
 console.log(targetNumber);
-console.log(gemValue)
+console.log(crystalValue);
+
 
 })
